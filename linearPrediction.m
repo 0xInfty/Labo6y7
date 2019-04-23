6a1956a5844912abde2863476147051aa569325b
0xInfty <anirix.vall@gmail.com>
2019-04-22 23:51:03 -0300

    Scripts 'linearPrediction.py' + 'linearPredicion.m'. F2 + S3 + D2 + A2.
    	Agrego archivo 'linearPrediction.m' para poder chequear paso a paso los comandos que transformo de Python a Matlab.
    
    Funcionales
    	Saco de momento el enunciado como función; después lo transformaré cuando funcione -valga la redundancia-.
    	Quito parámetros escritos al principio 'c1', ..., 'p2' que no cumplen función alguna.
    
    Sintaxis
    	Escribo algunas cosas de modo más compacto.
    	Renombro variables respecto a Matlab para que sea más claro -caso excepcionalmente confuso es que 'V' en Matlab es 'U' en Python para que no se confunda con el voltaje;
    mientras que 'U' en Matlab es 'eigenvectors' en Python.
    	Agrego muchos comentarios.
    
    Debug
    	Corrijo algunos productos matriciales, que en Python es mejor siempre escribir con 'np.matmul' por las dudas (A*B*C sería 'np.matmul(A, np.matmul(B, C))' por ej.).
    	Al principio había ordenado los autovalores de mayor a menor, pero resulta que hacía más confusos los productos U y A.
    
    Advertencias
    	¡Obtengo autovalores y autovectores complejos cuando quiero averiguar fase y amplitud! El código está escrito como para que funcione, pero hay que buscar una nueva función
    'eig' que no recaiga en estos errores -devuelve 5 autovalores reales y al hacerse menor el módulo, se vuelven complejos.
    	Faltan algunos parámetros estadísticos y los gráficos.

---
+++ linearPrediction.m
@@ -0,0 +1,182 @@
+clear all
+
+rawdata = importdata('Datos.txt');
+t = rawdata(1,:)';
+x = rawdata(2,:)';
+clear rawdata
+
+%%
+
+N = length(x);
+M = round(N*0.75);
+
+X = zeros(N-M,M);
+
+for i=1:M
+    for k=1:N-M
+        X(k,i)=x(i+k);
+    end
+end
+clear i k
+
+%%
+
+XX = X * X';
+[U, D] = eig(XX);
+d = eig(XX);
+
+for i = 1:length(d)
+    hey = U(1,i);
+    hello = U(:,i);
+    U(:,i) = hello/hey;
+end
+clear hello hey
+
+%%
+
+nsignificant = 4;
+
+F = zeros(nsignificant);
+
+for j=length(d)-nsignificant+1:length(d)
+%for j=1:size(D,1)
+  F(j,j)=1/sqrt(d(j));% 1 overlambda
+end
+    
+%%
+
+V=X'*U*F;
+xvector=x(1:N-M);
+A=V*F'*U'*xvector;
+
+%%
+
+c(1)=1;
+for i=1:length(A)
+    c(i+1)=-A(i);
+end
+r=roots(c);
+s=sort(r); % sorted from smallest to largest absolute value
+l=rank(D);
+
+%%
+
+for i=1:length(s)
+    ss(i)=s(length(s)-i+1);    
+end
+ss = ss' % sorted from largest to smallest absolute value
+
+%%
+
+dtime = 1;
+
+for j=1:l
+ 
+    b(j)=log(abs(ss(j)))/dtime;
+    w(j)=angle(ss(j))/dtime;
+
+end
+
+%%
+
+[P,I]=sort(w);
+Z=b(I);
+
+%%
+
+Nzeros=0;
+for j=1:l
+    if w(j)==0
+        Nzeros=Nzeros+1;
+    end
+end
+
+for i=1:round((l-Nzeros)/2+Nzeros)
+    WW(i)=abs(P(i));
+    BBB(i)=(Z(i));  
+end
+
+%%
+
+Npos=0;
+for j=1:length(BBB);
+    if BBB(j)>=0
+        Npos=Npos+1;
+    end
+end
+
+[B1,J]=sort(BBB);
+W1=WW(J);
+
+for i=1:length(B1)
+    B2(i)=B1(length(B1)-i+1);
+    W2(i)=W1(length(B1)-i+1);
+end
+
+for j=1:Npos
+    W(j)=W2(j);
+    B(j)=B2(j);
+end   
+
+%%
+
+% LS for amplitudes and phases
+
+% setup matrices
+
+for i=1:N
+    for j=1:length(W);
+        Xbar(i,2*j-1)=exp(-B(j)*(i-1)*dtime)*cos(W(j)*(i-1)*dtime);
+        Xbar(i,2*j)=-exp(-B(j)*(i-1)*dtime)*sin(W(j)*(i-1)*dtime);
+    end
+end
+
+
+%%
+
+XXbar = Xbar*Xbar';
+[Ubar,Dbar] = eig(XXbar);
+dbar = eig(XXbar);
+
+%%
+
+n1=1:length(dbar);
+%figure
+%plot(n1,dbar,'o')
+
+%n11=input('number of points lambda');
+
+n11=rank(XXbar);
+Fbar=zeros(n11);
+for j=length(dbar)-n11+1:length(dbar)
+
+  Fbar(j,j)=1/sqrt(dbar(j));% 1 overlambda
+    end
+
+
+Vbar=Xbar'*Ubar*Fbar;
+
+
+%least-squares
+
+AA=Vbar*Fbar'*Ubar'*x;
+
+%%
+
+for i=1:length(W)
+
+     if AA(2*i-1)==0 & AA(2*i)==0
+           C(i)=0;fi(i)=0;
+     elseif AA(2*i-1)==0
+            fi(i)=sign(AA(2*i))*pi/2;
+             C(i)=abs(AA(2*i));
+     elseif AA(2*i)==0
+             fi(i)=(1-sign(AA(2*i-1)))*pi/2;
+             C(i)=abs(AA(2*i-1));
+        else
+   fi(i)=atan2(AA(2*i),AA(2*i-1));
+ 
+    C(i)=sqrt(AA(2*i)^2+AA(2*i-1)^2);
+    end
+    end
+
--- linearPrediction.py
+++ linearPrediction.py
@@ -5,335 +5,198 @@ Created on Wed Apr 17 14:28:53 2019
 @author: LEC
 """
 
-import numpy as np
 from numpy import pi
-import matplotlib.pyplot as plt
+import numpy as np
+#import matplotlib.pyplot as plt
 
 #%%
 
-def linearPrediction(x, t, dt):
+# Get data
+t, x = np.loadtxt('Datos.txt')
+dt = 1
 
-    # Define general parameters
-    c1 = 2e-5
-    c2 = 1e-5
-    c3 = 0.5e-4
-    cn = 8e-15
-     
-    b1 = 3
-    b2 = 1
-    b3 = 5
+#%%
+
+# Define general parameters
+cn = 8e-15
+noise = []
+for i in range(50):
+    noise.append(np.cos(2*pi*np.random.rand() * (t+dt) / (2*dt)))
+noise = np.array(noise)
+noise = cn * sum(noise)
+coherent_artifact = 1e-15 * np.exp(-t**2 / 2 / 0.07**2)
+# Proporcional to cross-correlation
+
+"""
+Kind of answer we want:
+x = c1.*exp(-b1.*t).*cos(w1*t+p1) + c2.*exp(-b2.*t).*cos(w2.*t+p2) + 
+    + c3.*exp(-b3.*t.^2) + noise + coherent_artifact;
+"""
+
+#%%
+
+# -----------------------------------------------------------------------------
+# FREQUENCIES AND DAMPING FACTORS
+# -----------------------------------------------------------------------------
+
+# Create data matrix
+N = len(x)
+M = round(0.75 * N)
+X = np.array([x[i+j+1] for j in range(N-M) for i in range(M)]).reshape((N-M,M))
+
+# Diagonalize square data matrix
+[eigenvalues, eigenvectors] = np.linalg.eig( np.matmul(X, X.T) )
+ordered_index = eigenvalues.argsort() # From smallest to largest value 
+eigenvalues = eigenvalues[ordered_index] # Eigenvalues
+eigenvectors = eigenvectors[:, ordered_index] # Eigenvectors on columns
+eigenvectors = np.array([l/l[0] for l in eigenvectors.T]).T # Normalize
+rank = np.linalg.matrix_rank(np.diag(eigenvalues)) # Size measure
+
+"""
+La segunda columna de eV la tengo con signo invertido respecto a Matlab.
+==> Obtengo resultados distintos para U.
+"""
+
+#%%
+
+# Choose number of significant values
+Nsignificant = 4
+#plt.figure()
+#plt.semilogy(eigenvalues, linestyle='none', marker='o', 
+#             fillstyle='none', markersize=10)
+#plt.xlabel("Número")
+#plt.ylabel("Autovalores")
+#Nsignificant = int(input('¿Número de valores singulares?\t'))
+
+# Crop data according to it
+F = np.zeros((N-M, N-M))
+F[-Nsignificant:,-Nsignificant:] = np.diag(1/np.sqrt(eigenvalues[-Nsignificant:]))
+auxiliar = np.matmul(eigenvectors, F)
+U = np.matmul(X.T, auxiliar) # Xmatrix.T * eigenvectors * F
+
+#%%
+
+# Define polinomyal
+auxiliar = np.matmul(eigenvectors.T, x[:N-M])
+auxiliar = np.matmul(F.T, auxiliar)
+A = np.matmul(U, auxiliar) # U * F.T * eigenvectors.T * xvector 
+# |--> Least-Squares?
+coeficients = np.array([1, *list(-A)])
+
+# Solve and find its roots
+roots = np.roots(coeficients)
+ordered_index = abs(roots).argsort() 
+roots = roots[ordered_index][::-1] # From largest to smallest absolute value
+
+#%%
+
+# Calculate damping constants 'b' and frequencies 'omega'
+damping_constants = (np.log(abs(roots)) / dt)[:rank] # Crop them accordingly
+frequencies = (np.angle(roots) / dt)[:rank]
+
+#%%
+
+# Sort them
+ordered_index = frequencies.argsort() # From smallest to largest frequency
+frequencies = frequencies[ordered_index]
+damping_constants = damping_constants[ordered_index]
+
+# Crop them according to number of real roots and rank of diagonalized matrix
+Nzeros = len(frequencies) - np.count_nonzero(frequencies)
+frequencies = abs(frequencies)[:int(round((rank-Nzeros)/2+Nzeros))]
+damping_constants = damping_constants[:int(round((rank-Nzeros)/2+Nzeros))]
+
+# Then crop them according to the number of positive or zero damping constants
+Npositives = len(damping_constants[damping_constants>=0])
+ordered_index = damping_constants.argsort()[::-1] # From largest to smallest
+damping_constants = damping_constants[ordered_index][:Npositives]
+frequencies = frequencies[ordered_index][:Npositives]
+
+# Now I have the smallest frequencies and largest damping constants
+
+#%%
+
+# -----------------------------------------------------------------------------
+# AMPLITUDES AND PHASES
+# -----------------------------------------------------------------------------
+
+# Create modelled data matrix
+Nsolutions = len(frequencies)
+t2 = np.arange(0, N*dt, dt) # Time starting on zero
+X2 = np.zeros((N, 2*Nsolutions))
+for i, b, omega in zip(range(Nsolutions), damping_constants, frequencies):
+    X2[:, 2*i] = np.exp(-b*t2) * np.cos(omega*t2)
+    X2[:, 2*i+1] = -np.exp(-b*t2) * np.sin(omega*t2)
+
+#%%
+
+# Diagonalize square modelled data matrix
+[eigenvalues2, eigenvectors2] = np.linalg.eig( np.matmul(X2, X2.T) )
+ordered_index = abs(eigenvalues2).argsort() # From smallest to largest absolute
+eigenvalues2 = eigenvalues2[ordered_index] # Eigenvalues
+eigenvectors2 = eigenvectors2[:, ordered_index] # Eigenvectors on columns
+eigenvectors2 = np.array([l/l[0] for l in eigenvectors2.T]).T # Normalize
+rank2 = np.linalg.matrix_rank(np.diag(eigenvalues2)) # Size measure
+
+# $&@%!! COMPLEX EIGENVALUES AND EIGENVECTORS >.<
+# WE REALLY, REALLY HAVE TO LOOK FOR ANOTHER METHOD!
+
+#%%
+
+# Choose number of significant values
+Nsignificant2 = np.linalg.matrix_rank( np.matmul(X2, X2.T) )
+
+# Crop data according to it
+F2 = np.zeros((N, N))
+F2[-Nsignificant2:,-Nsignificant2:] = np.diag(
+        1/np.sqrt(abs(eigenvalues2[-Nsignificant2:])))
+auxiliar = np.matmul(eigenvectors2, F2)
+U2 = np.matmul(X2.T, auxiliar) # Xmatrix.T * eigenvectors * F
+
+#%%
+
+# Get defining vector
+auxiliar = np.matmul(eigenvectors2.T, x)
+auxiliar = np.matmul(F2.T, auxiliar)
+A2 = np.matmul(U2, auxiliar) # U * F.T * eigenvectors.T * xvector 
+# |--> Least-Squares?
+
+#%%
+
+# Calculate phases 'phi' and amplitudes 'C'
+amplitudes = []
+phases = []
+for i in range(Nsolutions):
     
-    w1 = 5*2*pi
-    w2 = 4.5*2*pi
-    
-    p1 = pi
-    p2 = 0.5*pi
-    
-    # Generate noise and coherent artifect
-    noise = []
-    for i in range(50):
-        noise.append(np.cos(2*pi*np.random.rand() * (t+dt) / (2*dt)))
-    noise = np.array(noise)
-    noise = cn * sum(noise)
-    coherent_artifact = 1e-15 * np.exp(-t**2 / 2 / 0.07**2)
-    # Proporcional to croscorrelation
-    
-    """
-    Kind of answer we want:
-    x = c1.*exp(-b1.*t).*cos(w1*t+p1) + c2.*exp(-b2.*t).*cos(w2.*t+p2) + 
-        + c3.*exp(-b3.*t.^2) + noise + coherent_artifact;
-    """
+    if A2[2*i]==0 and A2[2*i+1]==0:
+        amplitudes[i] == 0
+        phases[i] == 0
+    elif A2[2*i]==0:
+        amplitudes[i] = abs(A2[2*i+1])
+        phases[i] = np.sign(A2[2*i+1]) * pi/2
+    elif A2[2*i+1]==0:
+        amplitudes[i] = abs(A2[2*i])
+        phases[i] = (1-np.sign(A2[2*i])) * pi/2
+    else:
+        amplitudes[i] = np.sqrt(A2[2*i+1]**2 + A2(2*i)**2)
+        phases[i] = np.arctan2(A2[2*i+1], A2(2*i))
 
-    # Set up matrix from data (N-M) x M. We take M=0.75*N backward prediction
-    N = len(t)
-    M = round(0.75 * N)
-    X = np.zeros( (N-M, M) )
-    for i in range(M):
-        for k in range(N-M):
-            X[k,i] = x[i+k+1]
+#%%
 
-    # Computation of the (N-M)x(N-M) noonegativmatrix XX' and diagonalization
-    [ev, eV] = np.linalg.eig( np.matmul(X, X.T) )
-    ordered_index = ev.argsort()
-    ev = ev[ordered_index] # eigenvalues
-    eV = eV[:, ordered_index] # eigenvectors on columns
-    eD = np.diag(ev) # diagonal containing eigenvalues
+# -----------------------------------------------------------------------------
+# SOLUTION, PLOTS AND STATISTICS
+# -----------------------------------------------------------------------------
 
-    # XX is np.matmul(X, X.T)
-    # U is ev eigenvalues
-    # D is eV eigenvectors
-    # noo is nsignificant
+# Solution
+solutions = np.array([a * np.exp(-b*t) * np.cos(omega*t + fi)
+                      for a, b, omega, fi in zip(amplitudes,
+                                                 damping_constants,
+                                                 frequencies,
+                                                 phases)]).T
+total_solution = sum(solutions.T)
 
-    # Choose number of singular values    
-    plt.figure()
-    plt.semilogy(ev, linestyle='none', marker='o', 
-                 fillstyle='none', markersize=10)
-    plt.xlabel("Número")
-    plt.ylabel("Autovalores")
-    nsignificant = int(input('¿Número de valores singulares?\t'))
-    
-    F = np.diag(1/np.sqrt(ev[:nsignificant]))
-    U = np.matmul(X, eV*F)
+# Statistics
+square_chi = sum( (total_solution - x)**2 ) / N
+residue = x - total_solution
 
-#    #computation of LP coeficients
-#    
-#    xvector=x(1:N-M);
-#    A=V*F'*U'*xvector;
-#    
-#    #polynomial roots
-#    #c=zeros(length(A));
-#    
-#      c(1)=1;
-#    
-#    for i=1:length(A)
-#      c(i+1)=-A(i);
-#             end
-#             
-#             r=roots(c);
-#             
-#    
-#     #only l roots are significant being l rank of D matrix
-#             
-#             l=rank(D);
-#    
-#       s=sort(r);
-#    
-#        
-#     
-#       BB=length(s);
-#             
-#      #roots sorted in descending order
-#    
-#             for i=1:BB
-#     
-#              ss(i)=s(BB-i+1);
-#     
-#              end
-#    
-#    
-#    #S=1;
-#    
-#       #for i=1:length(r)
-#       # if abs(ss(i))>=1
-#           #S=S+1;
-#         #end
-#         #end 
-#     
-#                ss=ss';
-#                
-#             for j=1:l
-#    
-#                b(j)=log(abs(ss(j)))/dtime;
-#                w(j)=angle(ss(j))/dtime;
-#    
-#                     end
-#                  
-#             [P,I]=sort(w);
-#             Z=b(I);
-#    
-#           Nzeros=0;
-#             for j=1:l
-#               if w(j)==0
-#               Nzeros=Nzeros+1;
-#              end
-#             end
-#             Nzeros;
-#    
-#            for i=1:round((l-Nzeros)/2+Nzeros)
-#             WW(i)=abs(P(i));
-#             BBB(i)=(Z(i));  
-#             end
-#         #counting for positive damping constants  
-#         Npos=0;
-#           for j=1:length(BBB);
-#              if BBB(j)>=0
-#               Npos=Npos+1;
-#              end
-#            end
-#    
-#           [B1,J]=sort(BBB);
-#             W1=WW(J);
-#    
-#    
-#             
-#      # sorted in descending order
-#    
-#             for i=1:length(B1)
-#     
-#              B2(i)=B1(length(B1)-i+1);
-#     
-#              W2(i)=W1(length(B1)-i+1);
-#    
-#              end
-#    
-#            for j=1:Npos
-#               W(j)=W2(j);
-#              B(j)=B2(j);
-#              end    
-#             W;
-#    
-#             B;
-#             
-#             # LS for amplitudes and phases
-#             
-#             #setup matrices
-#       
-#            for i=1:N
-#    
-#                for j=1:length(W);
-#                   Xbar(i,2*j-1)=exp(-B(j)*(i-1)*dtime)*cos(W(j)*(i-1)*dtime);
-#                   Xbar(i,2*j)=-exp(-B(j)*(i-1)*dtime)*sin(W(j)*(i-1)*dtime);
-#                 
-#                end
-#             end
-#            
-#         
-#               XXbar=Xbar*Xbar';
-#    [Ubar,Dbar] = eig(XXbar);
-#    dbar=eig(XXbar);
-#    
-#    n1=1:length(dbar);
-#    #figure
-#    #plot(n1,dbar,'o')
-#    
-#    #n11=input('number of points lambda');
-#    
-#    n11=rank(XXbar);
-#    Fbar=zeros(n11);
-#    for j=length(dbar)-n11+1:length(dbar)
-#    
-#      Fbar(j,j)=1/sqrt(dbar(j));# 1 overlambda
-#        end
-#    
-#    
-#    Vbar=Xbar'*Ubar*Fbar;
-#    
-#    
-#    #least-squares
-#    
-#    AA=Vbar*Fbar'*Ubar'*x;
-#    
-#    
-#    
-#    for i=1:length(W)
-#    
-#         if AA(2*i-1)==0 & AA(2*i)==0
-#               C(i)=0;fi(i)=0;
-#         elseif AA(2*i-1)==0
-#                fi(i)=sign(AA(2*i))*pi/2;
-#                 C(i)=abs(AA(2*i));
-#         elseif AA(2*i)==0
-#                 fi(i)=(1-sign(AA(2*i-1)))*pi/2;
-#                 C(i)=abs(AA(2*i-1));
-#            else
-#       fi(i)=atan2(AA(2*i),AA(2*i-1));
-#     
-#        C(i)=sqrt(AA(2*i)^2+AA(2*i-1)^2);
-#        end
-#        end
-#    
-#    
-#    
-#    fi;
-#    C;
-#    
-#    
-#    
-#    for i=1:length(W)
-#       #if W(i)<40
-#          yy(:,i)=C(i).*exp(-B(i).*t).*cos(W(i).*t+fi(i));
-#       #else
-#       #      yy(:,i)=0*t;
-#       #end
-#    end
-#    
-#    
-#    yy=yy';
-#    Y=sum(yy);
-#    Chi2=sum((Y-x').^2)/N
-#    #figure
-#    #figure(2);
-#    subplot(3,2,2);
-#    plot(t,Y,t,x);title('Fit and Data');xlabel('time(ps)')
-#    
-#    
-#    results=[W 
-#      B
-#      fi
-#      C];
-#    
-#    results=results';
-#    
-#    # up to here the programm, ahead calculations to evaluate if the fit is good enough
-#    
-#    
-#    #calculation of the residue for evaluating the fit
-#    
-#    Y=Y';
-#    yy=yy';
-#    residue=x-Y;
-#    #figure;plot(t,residue);title('Residue');xlabel('time(ps)');ylabel('Data-Fit');
-#    
-#    #FFt of the residue
-#    
-#    #Y1 = fft(residue,4096);
-#    Y1=fft(residue);
-#    NN=length(Y1);
-#    Pyy= Y1.*conj(Y1)/(NN-1);
-#    frequ = 1/(t(2)-t(1))/NN*(0:((NN/2)-1))*100/3*30;#in GHz
-#    FFTResidue=Pyy(1:NN/2);
-#    frequ=frequ';
-#    #figure;
-#    subplot(3,2,4);
-#    plot(frequ,Pyy(1:NN/2));title('FFT of the Residue');xlabel('freq (GHz)');ylabel('FFT');
-#    
-#    #spectrum of the fit in a "Ramanlike" fashion, meaning without considering the phase or phase zero
-#    
-#    b=B/2/pi*100/3*30;#in GHz
-#    f=W*100/3/2/pi*30;#in GHz
-#    Wmax=max(W);
-#    freq=0:Wmax/2/pi/1000*100/3*30:1.5*Wmax/2/pi*100/3*30;#in GHz
-#    freq=freq';
-#    
-#    res=zeros(length(freq),length(W));
-#    
-#    for i=1:length(W)
-#       if W(i)==0
-#          res(:,i)=0;
-#       else 
-#          #res(:,i)=C(i)*imag(1i*exp(-1i*fi(i))*f(i)./(f(i)^2-freq.^2-2j.*freq*b(i)));
-#          res(:,i)=C(i)*imag(1*f(i)./(f(i)^2-freq.^2-2j.*freq*b(i)));
-#      end
-#      end
-#      
-#      spectrum=sum(res,2);
-#    #figure;
-#    subplot(3,2,6);
-#    plot(freq,spectrum,freq,res);title('Raman-like Data Spectrum');xlabel('freq (GHz)');ylabel('spectrum');
-#    
-#    
-#    #for SiO2
-#    t0=0.26;
-#    Cp=C.*exp(B*t0);
-#    fip=W*t0-fi;
-#    
-#    for i=1:length(W)
-#          yp(:,i)=Cp(i).*exp(-B(i).*t).*cos(W(i).*t+fip(i));
-#       end
-#    
-#    
-#    
-#    #Los resultados en GHz
-#    tau=1./B;#in picoseconds
-#    fG=W*100/3/2/pi*30;#in GHz
-#    Q=W/2./B;#factor de calidad
-#    
-#    'Frecuencia en GHz, tau en ps, fase en grados, Amplitud, Q'
-#    format shortE
-#    resultsGHz=[fG 
-#       tau
-#       fi*180/pi
-#       C
-#       Q];
-#    resultsGHz=resultsGHz'
-#    
+# MISSING STATISTICS AND PLOTS BUT THAT'S IT :)
