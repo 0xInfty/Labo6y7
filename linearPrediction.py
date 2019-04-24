5f622981e5c138529bebf2414237fc34e16c2233
0xInfty <anirix.vall@gmail.com>
2019-04-24 14:08:23 -0300

    Script 'linearPrediction.py'. S1 + A3.
    Sintaxis
    	Actualizo nombres para que sean más claros:
    		'solutions' es 'fit_terms';
    		'Nsolutions' es 'Nfit_terms';
    		'total_solution' es 'fit'.
    
    Aclaraciones
    	Llegamos a la conclusión de que no hace falta dividir las frecuencias de la FFT por 2*pi.
    	Sigue sin funcionar igual la FFT del residuo, pero no supimos determinar por qué y decidimos conservarla como lo indica los ejemplos de 'np.fft.rfft' y 'np.fft.rfftfreq'.
    	¡Ahora no funciona bien! Tira menos respuestas de las que debería (2 en lugar de 3).

--- linearPrediction.py
+++ linearPrediction.py
@@ -118,10 +118,10 @@ frequencies = frequencies[ordered_index][:Npositives]
 # -----------------------------------------------------------------------------
 
 # Create modelled data matrix
-Nsolutions = len(frequencies)
+Nfit_terms = len(frequencies)
 t2 = np.arange(0, N*dt, dt) # Time starting on zero
-X2 = np.zeros((N, 2*Nsolutions))
-for i, b, omega in zip(range(Nsolutions), damping_constants, frequencies):
+X2 = np.zeros((N, 2*Nfit_terms))
+for i, b, omega in zip(range(Nfit_terms), damping_constants, frequencies):
     X2[:, 2*i] = np.exp(-b*t2) * np.cos(omega*t2)
     X2[:, 2*i+1] = -np.exp(-b*t2) * np.sin(omega*t2)
 
@@ -132,7 +132,7 @@ for i, b, omega in zip(range(Nsolutions), damping_constants, frequencies):
 ordered_index = eigenvalues2.argsort() # From smallest to largest absolute
 eigenvalues2 = eigenvalues2[ordered_index] # Eigenvalues
 eigenvectors2 = eigenvectors2[:, ordered_index] # Eigenvectors on columns
-#eigenvectors2 = np.array([l/l[0] for l in eigenvectors2.T]).T # Normalize
+eigenvectors2 = np.array([l/l[0] for l in eigenvectors2.T]).T # Normalize
 rank2 = np.linalg.matrix_rank(np.diag(eigenvalues2)) # Size measure
 
 #%%
@@ -160,7 +160,7 @@ A2 = np.matmul(U2, auxiliar) # U * F.T * eigenvectors.T * xvector
 # Calculate phases 'phi' and amplitudes 'C'
 amplitudes = []
 phases = []
-for i in range(Nsolutions):
+for i in range(Nfit_terms):
     
     if A2[2*i]==0 and A2[2*i+1]==0:
         amplitudes.append( 0 )
@@ -184,24 +184,33 @@ phases = np.array(phases)
 # -----------------------------------------------------------------------------
 
 # Solution
-solutions = np.array([a * np.exp(-b*t) * np.cos(omega*t + phi)
-                      for a, b, omega, phi in zip(amplitudes,
+fit_terms = np.array([a * np.exp(-b*t) * np.cos(omega*t + phi)
+                     for a, b, omega, phi in zip(amplitudes,
                                                  damping_constants,
                                                  frequencies,
                                                  phases)]).T
-total_solution = sum(solutions.T)
-square_chi = sum( (total_solution - x)**2 ) / N # Best if absolute is smaller
+fit = sum(fit_terms.T)
+square_chi = sum( (fit - x)**2 ) / N # Best if absolute is smaller
 
 #%%
 
 # Statistics of the residue
-residue = x - total_solution
+residue = x - fit
 residue_transform = np.fft.rfft(residue)
-residue_frequencies = 1000 * np.fft.rfftfreq(N, d=dt) / (2*pi) # in GHz
+residue_frequencies = 1000 * np.fft.rfftfreq(N, d=dt) # in GHz
 plt.plot(residue_frequencies, residue_transform)
 
+"""
+Y1=fft(residue);
+NN=length(Y1);
+Pyy= Y1.*conj(Y1) / (NN-1);
+frequ = 1/(NN*(t(2)-t(1))) * (0:((NN/2)-1)) * 1000; %in GHz
+FFTResidue=Pyy(1:NN/2);
+plot(frequ, Pyy(1:NN/2))
+"""
+
 # I BELIEVE THE ORIGINAL FFT WAS FAULTY!
-# DOES THIS CARRY 2PI OR NOT?
+# OURS GOES UP TO 500 AND THE OLD ONE GOES TO 250.
 
 #%%
 
@@ -212,8 +221,8 @@ f_max = max(f_frequencies)
 f_independent = np.arange(0, 1.5*f_max, f_max/1000)
 
 # Raman-like Spectrum per se
-response = np.zeros( (len(f_independent), Nsolutions) )
-for i in range(Nsolutions):
+response = np.zeros( (len(f_independent), Nfit_terms) )
+for i in range(Nfit_terms):
    if frequencies[i]==0:
       response[:,i] = 0
    else:
@@ -223,5 +232,29 @@ for i in range(Nsolutions):
 spectrum = np.sum(response, axis=1)
 
 plt.plot(f_independent, spectrum)
+
+"""
+b = B * 1000 / (2*pi); %in GHz
+f = W * 1000 / (2*pi); %in GHz
+Wmax = max(f);
+freq = 0 : fMAX/1000 : 1.5*fMAX; %in GHz
+freq=freq';
+
+res = zeros(length(freq), length(W));
+
+for i=1:length(W)
+   if W(i)==0
+      res(:,i)=0;
+   else 
+      %res(:,i)=C(i)*imag(1i*exp(-1i*fi(i))*f(i)./(f(i)^2-freq.^2-2j.*freq*b(i)));
+      res(:,i)=C(i)*imag(1*f(i)./(f(i)^2-freq.^2-2j.*freq*b(i)));
+  end
+  end
+  
+  spectrum=sum(res,2);
+%figure;
+subplot(3,2,6);
+plot(freq,spectrum,freq,res);title('Raman-like Data Spectrum');xlabel('freq (GHz)');ylabel('spectrum');
+"""
 
 # THIS PART DOES DEFINITELY NOT WORK >.<
