38782ba9dfda4a713aca9aa4e6ad399346df4c10
0xInfty <anirix.vall@gmail.com>
2019-05-28 16:49:47 -0300

    Script 'PlotNFit'. F4.
    Funcional:
    	Ahora los parámetros se agrupan en 'ivu.InstancesDict', lo cual permite llamar sus elementos con punto en vez de con strings.
    	Ahora puedo elegir 't0' o no elegirlo, usando 'choose_t0'.
    	Me adapto a los cambios en 'linearPrediction' y 'linearPredictionPlot'.
    	Me adapto a que ahora 'copy' está en el módulo 'utilities'.

--- S_PlotNFit.py
+++ S_PlotNFit.py
@@ -8,29 +8,36 @@ Created on Wed Apr 17 14:28:53 2019
 import iv_analysis_module as iva
 import iv_plot_module as ivp
 import iv_save_module as ivs
+import iv_utilities_module as ivu
 import os
 import numpy as np
 
 #%% PARAMETERS -------------------------------------------------------------------
 
 # Parameters
-name = 'M_20190508_03'
+name = 'M_20190408_04'
 path = r'C:\Users\Luciana\Desktop\Vale e Iván\Mediciones\2019-04-08'
 
 # Plot parameters
-plot = False
-interactive = False
-autoclose = True
-autosave = True
+plot_params = dict(
+        plot = False,
+        interactive = False,
+        autoclose = True,
+        autosave = True
+        )
+plot_params = ivu.InstancesDict(plot_params)
 
 # Fit parameters
-parameters = dict(
+fit_params = dict(
         round_Matlab_needed = True, # Pyhon 3.6.2 needs it
         use_full_mean = True,
         use_experiments = [0], # First is 0, not 1!
         send_tail_to_zero = False,
         use_fraction = .2,
-        choose_tf = False)
+        choose_t0 = True,
+        choose_tf = False
+        )
+fit_params = ivu.InstancesDict(fit_params)
 
 # Create full filename
 filename = os.path.join(path, name+'.txt')
@@ -38,8 +45,10 @@ filename = os.path.join(path, name+'.txt')
 #%% PLOT -------------------------------------------------------------------------
 
 # Plot
-if plot:
-    fig_plot = ivp.plotPumpProbe(filename, interactive=interactive, autosave=autosave)
+if plot_params.plot:
+    ivp.plotPumpProbe(filename, 
+                      interactive=plot_params.interactive, 
+                      autosave=plot_params.autosave)
 
 # Several plots
 #ivp.plotAllPumpProbe(path, autosave=autosave, autoclose=autoclose)
@@ -49,41 +58,44 @@ if plot:
 # Load data
 t, V, details = ivs.loadNicePumpProbe(filename)
 
-# Choose initial time t0
-t0 = ivp.interactiveTimeZero(filename, autoclose=autoclose)
-t, V = iva.cropData(t0, t, V)
-
-# Choose final time tf
-if parameters['choose_tf']:
-    tf = ivp.interactiveTimeZero(filename, autoclose)
+# Choose time interval to fit
+if fit_params.choose_t0: # Choose initial time t0
+    t0 = ivp.interactiveTimeSelector(filename, autoclose=plot_params.autoclose)
+    t, V = iva.cropData(t0, t, V)
+else:
+    t0 = t[0]
+if fit_params.choose_tf: # Choose final time tf
+    tf = ivp.interactiveTimeSelector(filename, autoclose=plot_params.autoclose)
     t, V = iva.cropData(tf, t, V, logic='<=')
 else:
     tf = t[-1]
-parameters['time_range'] = (t0, tf)
+fit_params.time_range = (t0, tf)
+del t0, tf
 
 # Choose data to fit
-if parameters['use_full_mean']:
+if fit_params.use_full_mean:
     data = np.mean(V, axis=1)
 else:
-    data = np.mean(V[:, parameters['use_experiments']], axis=1)
+    data = np.mean(V[:, fit_params.use_experiments], axis=1)
 
 # Make a vertical shift
-if parameters['send_tail_to_zero']:
-    V0 = min(data[int( (1-parameters['use_fraction']) * len(data)):]) 
+if fit_params.send_tail_to_zero:
+    V0 = min(data[int( (1-fit_params.use_fraction) * len(data)):]) 
 else:
     V0 = 0
 data = data - V0
-parameters['V0'] = V0
+fit_params.voltage_zero = V0
+del V0
 
 # Use linear prediction
-results, others = iva.linearPrediction(
+results, other_results, plot_results = iva.linearPrediction(
     t, data, details['dt'], 
-    autoclose=autoclose,
-    round_Matlab_needed=parameters['round_Matlab_needed'])
+    autoclose=plot_params.autoclose,
+    round_Matlab_needed=fit_params.round_Matlab_needed)
 
 # Plot linear prediction
-fig_fit = ivp.linearPredictionPlot(filename, others, autosave=autosave)
+ivp.linearPredictionPlot(filename, plot_results, autosave=plot_params.autosave)
 
 # Generate fit tables
-tables = iva.linearPredictionTables(parameters, results, others)
-iva.copy(tables[0])
+tables = iva.linearPredictionTables(fit_params, results, other_results)
+ivu.copy(tables[0])
