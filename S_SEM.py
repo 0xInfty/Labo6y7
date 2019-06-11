43e78880a1b420bd3fb4ed9172b5f8586de96166
0xInfty <anirix.vall@gmail.com>
2019-06-11 20:12:59 -0300

    Script 'SEM'. F4.
    Funcional:
    	Ahora arregla las mediciones hechas en sentido contrario
    (aquellas que en vez de ser 90 son -90, por ej., porque medimos de
    izquierda a derecha en vez de derecha a izquierda en el ImageJ).
    	Ahora devuelve ángulos entre 0 y 180 :D
    	Ahora guarda los ángulos entre los resultados.
    	Ahora redondea los resultados de la tabla de OneNote a 2 cifras
    significativas en el error utilizando 'ivu.errorValue'.

--- S_SEM.py
+++ S_SEM.py
@@ -11,7 +11,7 @@ import numpy as np
 import os
 
 # Parameters
-path = r'C:\Users\quimica\Documents\Laboratorio\Profesores\Valeria Pais\Facu\OneDrive\Labo 6 y 7\Muestras\SEM\LIGO1\LIGO1 Geometrías\1'
+path = r'C:\Users\Usuario\OneDrive\Labo 6 y 7\Muestras\SEM\LIGO1\LIGO1 Geometrías\1'
 series = 'LIGO1_1'
 
 # Load data
@@ -22,7 +22,6 @@ width = []
 hangle = []
 wangle = []
 for file in os.listdir(path):
-#    print(file)
     if file.endswith("W.csv"):
         rwidth.append(file.split('_W.csv')[0].split('_')[-1])
         width.append(np.loadtxt(os.path.join(path, file), 
@@ -48,17 +47,67 @@ height = np.array(height).T
 width = np.array(width).T
 del file, rwidth, rheight
 
-# Organize angle data
+# Organize angle data...
+
+# ...1st fix the horizontal angles measured upside down
+new_hangle = []
+for ha in hangle:
+    new_ha = []
+    for i in ha:
+        difference = i - np.mean(ha)
+        if abs(difference)>90:
+            if abs(difference-180) < abs(difference+180):
+                new_ha.append(i-180)
+            else:
+                new_ha.append(i+180)
+        else:
+            new_ha.append(i)
+    new_hangle.append(new_ha)
+    del new_ha, i
+hangle = np.array(new_hangle).T
+del new_hangle
+
+# ...2nd fix the vertical angles measured upside down
 new_wangle = []
-for ha, wa in zip(hangle, wangle):
+for wa in wangle:
+    new_wa = []
+    for j in wa:
+        difference = np.mean(wa) - j
+        if abs(difference)>90:
+            if abs(difference-180) < abs(difference+180):
+                new_wa.append(j-180)
+            else:
+                new_wa.append(j+180)
+        else:
+            new_wa.append(j)
+    new_wangle.append(new_wa)
+    del new_wa, j
+wangle = np.array(new_wangle).T
+del new_wangle
+
+# ...3rd rotate vertical angles to be horizontal ones
+new_wangle = []
+for ha, wa in zip(hangle.T, wangle.T):
     difference = np.mean(ha) - np.mean(wa)
-    print(difference)
     if abs(difference-90) < abs(difference+90):
         new_wangle.append(wa + 90)
     else:
         new_wangle.append(wa - 90)
-angle = np.array([[*ha, *wa] for ha, wa in zip(hangle, new_wangle)]).T
-del wangle, hangle, new_wangle
+wangle = np.array(new_wangle).T
+del ha, wa, difference, new_wangle
+
+# ...4th make all angles point between 0 and 135
+angle = np.array([[*ha, *wa] for ha, wa in zip(hangle.T, wangle.T)]).T
+new_angle = []
+for a in angle.T:
+    if np.mean(a) < 0:
+        new_angle.append(a + np.ones(len(a))*180)
+    elif np.mean(a) > 180:
+        new_angle.append(a - np.ones(len(a))*180)
+    else:
+        new_angle.append(a)
+angle = np.array(new_angle).T
+del wangle, hangle, new_angle
 
 # Get results
 W = np.mean(width, axis=0)
@@ -67,26 +116,39 @@ H = np.mean(height, axis=0)
 dH = np.std(height, axis=0)
 A = W/H
 dA = W*dH/H**2 + dW/H
+a = np.mean(angle, axis=0)
+da = np.std(angle, axis=0)
 
 # Organize results
-results = np.array([W,dW,H,dH,A,dA]).T
+results = np.array([W,dW,H,dH,A,dA,a,da]).T
 heading = ["Ancho (nm)", "Error (nm)",
            "Altura (nm)", "Error (nm)",
-           "Relación de aspecto", "Error"]
+           "Relación de aspecto", "Error",
+           "Ángulo (°)", "Error (°)"]
 
 # Save data
 ivs.saveTxt(
     os.path.join(path,'Resultados_{}.txt'.format(series)), 
     results,
-    header=heading,
-    footer=dict(rods=rods)
+    header=heading, footer=dict(rods=rods),
+    overwrite=True
     )
 
+# Round and gather results
+items = []
+for i in range(len(rods)):
+    w = '\t'.join(ivu.errorValue(W[i], dW[i]))
+    h = '\t'.join(ivu.errorValue(H[i], dH[i]))
+    ra = '\t'.join(ivu.errorValue(A[i], dA[i], one_point_scale=True))
+    an = '\t'.join(ivu.errorValue(a[i], da[i]))
+    items.append('\t'.join([w, h, ra, an]))
+del w, h, ra, an, W, H, A, a, dW, dH, dA, da
+
 # Make OneNote table
-heading = ''.join(heading)
-items = ['\t'.join([str(element) for element in row]) for row in results]
+heading = '\t'.join(heading)
 items = ['\t'.join([n, r]) for n, r in zip(rods, items)]
 items = '\n'.join(items)
 heading = '\t'.join(['Rod', heading])
 table = '\n'.join([heading, items])
 ivu.copy(table)
+del heading, items
