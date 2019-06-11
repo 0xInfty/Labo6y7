63f5606a84b3d0892c07adbe728b6542e37b9523
0xInfty <anirix.vall@gmail.com>
2019-06-11 16:54:33 -0300

    Módulo 'utils'. F1.
    Funcional:
    	Retoco 'errorValue' y ahora funciona (aunque no del todo bien) devolviendo una lista de strings.

--- iv_utilities_module.py
+++ iv_utilities_module.py
@@ -176,9 +176,149 @@ class InstancesDict:
 
 #%%
 
-#def errorValue(X, dX, error_digits=2, units='',
-#               string_scale=True, one_point_scale=False, 
-#               legend=False, unpack=False):
+def errorValue(X, dX, error_digits=2, units='',
+               string_scale=True, one_point_scale=False):
+    
+    """Rounds up value and error of a measure. Returns list of strings.
+    
+    This function takes a measure and its error as input. Then, it 
+    rounds up both of them in order to share the same amount of decimal 
+    places.
+    
+    After that, it generates a latex string containing the rounded up 
+    measure. For that, it can rewrite both value and error so that the 
+    classical prefix scale of units can be applied.
+    
+    Parameters
+    ----------
+    X : float
+        Measurement's value.
+    dX : float
+        Measurement's associated error.
+    error_digits=2 : int, optional.
+        Desired number of error digits.
+    units='' : str, optional.
+        Measurement's units.
+    string_scale=True : bool, optional.
+        Whether to apply the classical prefix scale or not.        
+    one_point_scale=False : bool, optional.
+        Applies prefix with one order less.
+    
+    Returns
+    -------
+    string : str
+        Latex string containing value and error.
+    
+    Examples
+    --------
+    >> errorValue(1.325412, 0.2343413)
+    ['1.33', '0.23']
+    >> errorValue(1.325412, 0.2343413, error_digits=3)
+    ['1.325', '0.234']
+    >> errorValue(.133432, .00332, units='V')
+    ['133.4 mV', '3.3 mV']
+    >> errorValue(.133432, .00332, one_point_scale=True, units='V')
+    ['0.1334 V', '0.0033 V']
+    >> errorValue(.133432, .00332, string_scale=False, units='V')
+    ['1.334E-1 V', '0.033E-1 V']
+    
+    See Also
+    --------
+    copy
+    
+    """
+    
+    # First, I string-format the error to scientific notation with a 
+    # certain number of digits
+    if error_digits >= 1:
+        aux = '{:.' + str(error_digits) + 'E}'
+    else:
+        print("Unvalid 'number_of_digits'! Changed to 1 digit")
+        aux = '{:.0E}'
+    error = aux.format(dX)
+    error = error.split("E") # full error (string)
+    
+    error_order = int(error[1]) # error's order (int)
+    error_value = error[0] # error's value (string)
+
+    # Now I string-format the measure to scientific notation
+    measure = '{:E}'.format(X)
+    measure = measure.split("E") # full measure (string)
+    measure_order = int(measure[1]) # measure's order (int)
+    measure_value = float(measure[0]) # measure's value (string)
+    
+    # Second, I choose the scale I will put both measure and error on
+    # If I want to use the string scale...
+    if -12 <= measure_order < 12 and string_scale:
+        prefix = ['p', 'n', r'$\mu$', 'm', '', 'k', 'M', 'G']
+        scale = [-12, -9, -6, -3, 0, 3, 6, 9, 12]
+        for i in range(8):
+            if not one_point_scale:
+                if scale[i] <= measure_order < scale[i+1]:
+                    prefix = prefix[i] # prefix to the unit
+                    scale = scale[i] # order of both measure and error
+                    break
+            else:
+                if scale[i]-1 <= measure_order < scale[i+1]-1:
+                    prefix = prefix[i]
+                    scale = scale[i]
+                    break
+        used_string_scale = True
+    # ...else, if I don't or can't...
+    else:
+        scale = measure_order
+        prefix = ''
+        used_string_scale = False
+    
+    # Third, I actually scale measure and error according to 'scale'
+    # If error_order is smaller than scale...
+    if error_order < scale:
+        if error_digits - error_order + scale - 1 >= 0:
+            aux = '{:.' + str(error_digits - error_order + scale - 1)
+            aux = aux + 'f}'
+            error_value = aux.format(
+                    float(error_value) * 10**(error_order - scale))
+            measure_value = aux.format(
+                    measure_value * 10**(measure_order - scale))
+        else:
+            error_value = float(error_value) * 10**(error_order - scale)
+            measure_value = float(measure_value)
+            measure_value = measure_value * 10**(measure_order - scale)
+    # Else, if error_order is equal or bigger than scale...
+    else:
+        aux = '{:.' + str(error_digits - 1) + 'f}'
+        error_value = aux.format(
+                float(error_value) * 10**(error_order - scale))
+        measure_value = aux.format(
+                float(measure_value) * 10**(measure_order - scale))
+    
+    # Forth, I make a string for each of them. Ex.: '1.34 kV'    
+    string = [measure_value, error_value]
+    if not used_string_scale and measure_order != 0:
+        string = [st + 'E' + '{:.0f}'.format(scale) + ' ' + units 
+                  for st in string]
+    elif used_string_scale:
+        string = [st + ' ' + prefix + units for st in string]        
+    aux = []
+    for st in string:
+        if st[-1]==' ':
+            aux.append(st[:len(st)-1])
+        else:
+            aux.append(st)
+    string = aux
+    
+    return string
+
+"""
+>>> errorValue(.133432, .00332, string_scale=False, units='V', unpack=True)
+[]
+"""
+
+#%%
+
+#def errorValueLatex(X, dX, error_digits=2, units='',
+#                    string_scale=True, one_point_scale=False, 
+#                    legend=False, unpack=False):
 #    
 #    """Rounds up value and error of a measure. Also makes a latex string.
 #    
@@ -230,71 +370,7 @@ class InstancesDict:
 #    copy
 #    
 #    """
-#    
-#    # First, I string-format the error to scientific notation with a 
-#    # certain number of digits
-#    if error_digits >= 1:
-#        aux = '{:.' + str(error_digits) + 'E}'
-#    else:
-#        print("Unvalid 'number_of_digits'! Changed to 1 digit")
-#        aux = '{:.0E}'
-#    error = aux.format(dX)
-#    error = error.split("E") # full error (string)
-#    
-#    error_order = int(error[1]) # error's order (int)
-#    error_value = error[0] # error's value (string)
 #
-#    # Now I string-format the measure to scientific notation
-#    measure = '{:E}'.format(X)
-#    measure = measure.split("E") # full measure (string)
-#    measure_order = int(measure[1]) # measure's order (int)
-#    measure_value = float(measure[0]) # measure's value (string)
-#    
-#    # Second, I choose the scale I will put both measure and error on
-#    # If I want to use the string scale...
-#    if -12 <= measure_order < 12 and string_scale:
-#        prefix = ['p', 'n', r'$\mu$', 'm', '', 'k', 'M', 'G']
-#        scale = [-12, -9, -6, -3, 0, 3, 6, 9, 12]
-#        for i in range(8):
-#            if not one_point_scale:
-#                if scale[i] <= measure_order < scale[i+1]:
-#                    prefix = prefix[i] # prefix to the unit
-#                    scale = scale[i] # order of both measure and error
-#                    break
-#            else:
-#                if scale[i]-1 <= measure_order < scale[i+1]-1:
-#                    prefix = prefix[i]
-#                    scale = scale[i]
-#                    break
-#        used_string_scale = True
-#    # ...else, if I don't or can't...
-#    else:
-#        scale = measure_order
-#        prefix = ''
-#        used_string_scale = False
-#    
-#    # Third, I actually scale measure and error according to 'scale'
-#    # If error_order is smaller than scale...
-#    if error_order < scale:
-#        if error_digits - error_order + scale - 1 >= 0:
-#            aux = '{:.' + str(error_digits - error_order + scale - 1)
-#            aux = aux + 'f}'
-#            error_value = aux.format(
-#                    float(error_value) * 10**(error_order - scale))
-#            measure_value = aux.format(
-#                    measure_value * 10**(measure_order - scale))
-#        else:
-#            error_value = float(error_value) * 10**(error_order - scale)
-#            measure_value = float(measure_value)
-#            measure_value = measure_value * 10**(measure_order - scale)
-#    # Else, if error_order is equal or bigger than scale...
-#    else:
-#        aux = '{:.' + str(error_digits - 1) + 'f}'
-#        error_value = aux.format(
-#                float(error_value) * 10**(error_order - scale))
-#        measure_value = aux.format(
-#                float(measure_value) * 10**(measure_order - scale))
-#    
 #    # Forth, I make a latex string. Ex.: '(1.34$pm$0.32) kV'
 #    if not unpack:
 #        latex_str = r'({}$\pm${})'.format(measure_value, error_value)
@@ -330,8 +406,3 @@ class InstancesDict:
 #        latex_str = aux
 #    
 #    return latex_str
-
-"""
->>> errorValue(.133432, .00332, string_scale=False, units='V', unpack=True)
-[]
-"""
