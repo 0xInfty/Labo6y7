647f85149298036014bff418b1fb1a6b0cb81415
0xInfty <anirix.vall@gmail.com>
2019-06-24 19:47:52 -0300

    Script 'Frequencies'. F2 + D2.
    Funcional:
    	Agrego parámetro 'minimum_frequency' para filtrar outliers en
    frecuencia.
    	Agrego gráficos y ajuste lineal.
    
    Debug:
    	Arreglo la parte que armaba tabla y que guardaba nuevo archivo
    con todos los datos completos.
    	Dejo comentada la línea que gira los nombres de las nanoantenas.

--- S_Frequencies.py
+++ S_Frequencies.py
@@ -10,17 +10,19 @@ import matplotlib.pyplot as plt
 import os
 import iv_save_module as ivs
 import iv_utilities_module as ivu
+import iv_analysis_module as iva
 
-# PARAMETERS ------------------------------------------------------------------
+#%% PARAMETERS ----------------------------------------------------------------
 
 # Main folder's path
-home = r'C:\Users\quimica\Documents\Laboratorio\Profesores\Valeria Pais\Facu\OneDrive\Labo 6 y 7'
+home = r'C:\Users\Luciana\OneDrive\Labo 6 y 7'
 # Path to a list of filenames and rods to analize
 rods_filename = os.path.join(home, r'Análisis\Rods_LIGO1.txt')
 sem_filename = os.path.join(home, r'Muestras\SEM\LIGO1\LIGO1 Geometrías\1\Resultados_LIGO1_1.txt')
-desired_frequency = 9.5 # in GHz
+desired_frequency = 10 # in GHz
+minimum_frequency = 10
 
-# CODE ------------------------------------------------------------------------
+#%% LOAD DATA -----------------------------------------------------------------
 
 # Look for the list of rods and filenames
 filenames = [] # Will contain filenames like 'M_20190610_01'
@@ -60,41 +62,7 @@ del rod, fit, i
 fits_data = np.array([fit[i,:] for fit, i in zip(fits_data,index)])
 del index
 
-# Plot frequency, characteristic time and quality factor
-#plt.figure()
-#plt.subplot(3,1,1)
-#plt.plot(fits_data[:,0], 'o')
-#plt.subplot(3,1,2)
-#plt.plot(fits_data[:,2], 'o')
-#plt.subplot(3,1,3)
-#plt.plot(fits_data[:,1], 'o')
-
-# Plot results for the different rods
-fig, ax1 = plt.subplots()
-
-# Frequency plot, right axis
-ax1.set_xlabel('Antena')
-ax1.set_ylabel('Frecuencia (GHz)', color='tab:red')
-ax1.plot(fits_data[:,0], 'ro')
-ax1.tick_params(axis='y', labelcolor='tab:red')
-
-# Quality factor, left axis
-ax2 = ax1.twinx()  # Second axes that shares the same x-axis
-ax2.set_ylabel('Factor de calidad (u.a.)', color='tab:blue')
-ax2.plot(fits_data[:,2], 'bx')
-ax2.tick_params(axis='y', labelcolor='tab:blue')
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
-plt.show()
-
-# Format graph
-plt.xticks(np.arange(len(rods)), rods)
-plt.grid(which='both', axis='x')
-ax1.tick_params(length=2)
-#for tick in ax1.axes.get_xticklabels():
-#tick.set_visible(False)
-ax1.grid(axis='x', which='both')
-ax1.tick_params(axis='x', labelrotation=90)
-
+# Also load data from SEM dimension analysis
 other_data, other_header, other_footer = ivs.loadTxt(sem_filename)
 other_rods = other_footer['rods']
 new_data = []
@@ -103,29 +71,7 @@ for r in rods:
     new_data.append(other_data[i])
 sem_data = np.array(new_data)
 
-#%% ANALYSIS WITH ANDREA ------------------------------------------------------
-
-#plt.figure()
-#plt.plot(1/sem_data[:,2],fits_data[:,0],'o')
-#plt.ylabel('Frecuencia (GHz)')
-#plt.xlabel('1/Longitud (1/nm)')
-
-length = sem_data[:,2]
-young = 42e9 # E = 78 GPa
-young_2 = 79e9
-density = 19300 # kg/m3
-theory = (np.sqrt(young/density) / (2 * length*1e-9)) * 1e-9
-theory_2 = (np.sqrt(young_2/density) / (2 * length*1e-9)) * 1e-9
-
-plt.figure()
-plt.loglog(sem_data[:,2],fits_data[:,0],'o')
-plt.ylabel('Frecuencia (GHz)')
-plt.xlabel('Longitud (nm)')
-plt.loglog(length, theory, 'x')
-plt.loglog(length, theory_2, 'x')
-
-#%%
-
+# Prepare important data for a table 
 items = []
 for i in range(len(rods)):
     h = '\t'.join(ivu.errorValue(sem_data[i,2], sem_data[i,3]))
@@ -146,9 +92,132 @@ table = '\n'.join([heading, items])
 ivu.copy(table)
 del heading, items
 
-hole_data = np.array([*sem_data[:,:6], fits_data[:,0], fits_data[:,2]])
-ivs.saveTxt(hole_data, heading=["Ancho (nm)", "Error (nm)",
-                                "Longitud (nm)", "Error (nm)", 
-                                "Relación de aspecto", "Error",
-                                "Frecuencia (GHz)", "Factor de calidad"],
-            footer=dict(rods=rods))
+# Save all important data to a single file
+whole_filename = os.path.join(home, r'Análisis/Resultados_LIGO1_1.txt')
+whole_data = np.array([*sem_data[:,:6].T, fits_data[:,0], fits_data[:,2]])
+ivs.saveTxt(whole_filename, whole_data, 
+            header=["Ancho (nm)", "Error (nm)",
+                    "Longitud (nm)", "Error (nm)", 
+                    "Relación de aspecto", "Error",
+                    "Frecuencia (GHz)", "Factor de calidad"],
+            footer=dict(rods=rods),
+            overwrite=True)
+
+
+#%% ANALYSIS ------------------------------------------------------------------
+
+#%% 1) FREQUENCY AND QUALITY FACTOR PER ROD
+
+# Plot results for the different rods
+fig, ax1 = plt.subplots()
+
+# Frequency plot, right axis
+ax1.set_xlabel('Antena')
+ax1.set_ylabel('Frecuencia (GHz)', color='tab:red')
+ax1.plot(fits_data[:,0], 'ro')
+ax1.tick_params(axis='y', labelcolor='tab:red')
+
+# Quality factor, left axis
+ax2 = ax1.twinx()  # Second axes that shares the same x-axis
+ax2.set_ylabel('Factor de calidad (u.a.)', color='tab:blue')
+ax2.plot(fits_data[:,2], 'bx')
+ax2.tick_params(axis='y', labelcolor='tab:blue')
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.show()
+
+# Format graph
+plt.xticks(np.arange(len(rods)), rods, rotation='vertical')
+plt.grid(which='both', axis='x')
+ax1.tick_params(length=2)
+ax1.grid(axis='x', which='both')
+#ax1.tick_params(axis='x', labelrotation=90)
+
+#%% 2) FREQUENCY AND LENGTH
+
+#plt.figure()
+#plt.plot(1/sem_data[:,2],fits_data[:,0],'o')
+#plt.ylabel('Frecuencia (GHz)')
+#plt.xlabel('1/Longitud (1/nm)')
+
+length = sem_data[:,2]
+young = 42e9 # E = 78 GPa
+young_2 = 79e9
+density = 19300 # kg/m3
+theory = (np.sqrt(young/density) / (2 * length*1e-9)) * 1e-9
+theory_2 = (np.sqrt(young_2/density) / (2 * length*1e-9)) * 1e-9
+
+plt.figure()
+plt.loglog(length, fits_data[:,0],'o')
+plt.ylabel('Frecuencia (GHz)')
+plt.xlabel('Longitud (nm)')
+plt.loglog(length, theory, '-')
+plt.loglog(length, theory_2, '-')
+
+rsq, m, b = iva.linearFit(1/length, fits_data[:,0])
+plt.ylabel('Frecuencia (GHz)')
+plt.xlabel('Inverso de longitud (1/nm)')
+young_fit = 4 * density * (m[0]**2)
+young_fit_error = 8 * density * m[1] * m[0]
+print(r"Módulo de Young: {}".format(ivu.errorValueLatex(young_fit, 
+                                                        young_fit_error, 
+                                                        units="Pa")))
+
+#%% 3) FREQUENCY AND ASPECT RELATION
+
+# Plot results 
+fig, ax1 = plt.subplots()
+
+# Frequency vs width plot, lower axis
+ax1.set_xlabel('Ancho (nm)', color='tab:red')
+ax1.set_ylabel('Frecuencia (GHz)')
+ax1.plot(sem_data[:,0], fits_data[:,2], 'ro')
+ax1.tick_params(axis='x', labelcolor='tab:red')
+
+# Frequency vs quality factor, upper axis
+ax2 = ax1.twiny()  # Second axes that shares the same y-axis
+ax2.set_xlabel('Factor de calidad (u.a.)', color='tab:blue')
+ax2.plot(sem_data[:,4], fits_data[:,2], 'bx')
+ax2.tick_params(axis='x', labelcolor='tab:blue')
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.show()
+
+# Format graph
+ax1.grid(axis='both')
+                     
+#%% 4) FREQUENCY AND LENGTH WITHOUT OUTLIERS
+
+# Try it all again, removing outliers
+nice_rods = fits_data[:,0] >= minimum_frequency
+nice_fits_data = fits_data[nice_rods,:]
+nice_sem_data = sem_data[nice_rods, :]
+
+rsq, m, b = iva.linearFit(1/nice_sem_data[:,2], nice_fits_data[:,0])
+plt.ylabel('Frecuencia (GHz)')
+plt.xlabel('Inverso de longitud (1/nm)')
+young_fit = 4 * density * (m[0]**2)
+young_fit_error = 8 * density * m[1] * m[0]
+print(r"Módulo de Young: {}".format(ivu.errorValueLatex(young_fit, 
+                                                        young_fit_error, 
+                                                        units="Pa")))
+
+#%% 3) FREQUENCY AND ASPECT RELATION WITHOUT OUTLIERS
+
+# Plot results 
+fig, ax1 = plt.subplots()
+
+# Frequency vs width plot, lower axis
+ax1.set_xlabel('Ancho (nm)', color='tab:red')
+ax1.set_ylabel('Frecuencia (GHz)')
+ax1.plot(nice_sem_data[:,0], nice_fits_data[:,2], 'ro')
+ax1.tick_params(axis='x', labelcolor='tab:red')
+
+# Frequency vs quality factor, upper axis
+ax2 = ax1.twiny()  # Second axes that shares the same y-axis
+ax2.set_xlabel('Factor de calidad (u.a.)', color='tab:blue')
+ax2.plot(nice_sem_data[:,4], nice_fits_data[:,2], 'bx')
+ax2.tick_params(axis='x', labelcolor='tab:blue')
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.show()
+
+# Format graph
+ax1.grid(axis='both')
