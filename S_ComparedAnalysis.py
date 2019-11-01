f7b1818e87320e0a1528b8d8d1ffe69d89616c08
0xInfty <anirix.vall@gmail.com>
2019-11-01 12:29:43 -0300

    Script 'CA'. F2.
    Funcional:
    	Continúo adaptando 'ModelsNAnalysis' para poder comparar dos mediciones.
    	Completo tentativamente la carga de datos y commiteo para poder probarlo.

--- S_ComparedAnalysis.py
+++ S_ComparedAnalysis.py
@@ -9,7 +9,7 @@ import numpy as np
 #import matplotlib.pyplot as plt
 import os
 import iv_save_module as ivs
-#import iv_utilities_module as ivu
+import iv_utilities_module as ivu
 #import iv_analysis_module as iva
 
 #%% PARAMETERS ----------------------------------------------------------------
@@ -19,6 +19,7 @@ home = r'F:\Pump-Probe\Iván y Valeria\OneDrive\Labo 6 y 7'
 
 # For each data to compare, we need one value on each list
 desired_frequency = [12, 17] # in GHz
+full_series = ['Fused Silica + Aire', 'Fused Silica + Ta2O5']
 series = ['LIGO1_PostUSA', 'LIGO1']
 sem_series = ['LIGO1_1', 'LIGO1_1']
 
@@ -46,6 +47,13 @@ rods = []
 params = []
 fits_data = []
 fits_footer = []
+frequency = []
+damping_time = []
+quality_factor = []
+sem_data = []
+sem_footer = []
+length = []
+width = []
 
 for s, ss, f in zip(series, sem_series, desired_frequency):
 
@@ -78,14 +86,14 @@ for s, ss, f in zip(series, sem_series, desired_frequency):
                 spectral_width.append(float(line.split('\t')[-1]))
         del line
     sparams = np.array([amplitude, power, wavelength, spectral_width]).T
-    index = [params_filenames.index(f) for f in filenames]
+    index = [params_filenames.index(f) for f in sfilenames]
     sparams = sparams[index,:]
     params_header = ['Amplitud (mVpp)', 'Potencia Pump post-MOA (muW)', 
                      'Longitud de onda (nm)', 'Ancho medio de la campana (nm)']
     del params_filenames, index, amplitude, power, wavelength, spectral_width
     
     # Now create a list of folders for each filename    
-    fits_filenames = [ivs.filenameToFitsFilename(file, home) for file in filenames]
+    fits_filenames = [ivs.filenameToFitsFilename(file, home) for file in sfilenames]
     
     # Load data from each fit
     sfits_data = []
@@ -98,100 +106,106 @@ for s, ss, f in zip(series, sem_series, desired_frequency):
     
     # Keep only the fit term that has the closest frequency to the desired one
     fits_new_data = []
-    for rod, fit in zip(rods, sfits_data):
+    for rod, fit in zip(srods, sfits_data):
         try:
-            i = np.argmin(abs(fit[:,0] - desired_frequency*np.ones(fit.shape[0])))
+            i = np.argmin(abs(fit[:,0] - f*np.ones(fit.shape[0])))
             fits_new_data.append([*fit[i,:]])
         except IndexError:
             fits_new_data.append([*fit])
     sfits_data = np.array(fits_new_data)
-    frequency = sfits_data[:,0]*1e9 # Hz
-    damping_time = sfits_data[:,1]*1e-12 # s
-    quality_factor = sfits_data[:,2]
+    sfrequency = sfits_data[:,0]*1e9 # Hz
+    sdamping_time = sfits_data[:,1]*1e-12 # s
+    squality_factor = sfits_data[:,2]
     del rod, fit, i, fits_new_data
     
-    #### ACÁ ME QUEDEÉEEEEEEEE
-    
-    # Also create a list of folders for SEM filenames
-    sem_filenames = [filenameToSEMFilename(s) for s in sem_series]
-    
-    # Then load data from SEM dimension analysis
-    sem_data = []
-    sem_footer = []
-    sem_rods = []
-    for f in zip(sem_filenames, sem_series):
-        d, sem_header, f = ivs.loadTxt(sf)
-        r = [sem_short_series(s).format(rs) for rs in f['rods']]
-        sem_data.append(d)
-        sem_footers = sem_footers + [f]
-        sem_rods = sem_rods + r
-    del sf, s, d, f, r, sem_filenames
-    
+    # Also load data from SEM dimension analysis
+    ssem_data, sem_header, ssem_footer = ivs.loadTxt(semFilename(ss))
     other_data = []
-    for s in sem_data:
-        for si in s:
-            other_data.append([*si])
+    for d in ssem_data:
+        for di in d:
+            other_data.append([*di])
     other_data =  np.array(other_data)
-    del s, si
+    del d, di
     
-    index = [sem_rods.index(r) for r in rods]
-    sem_data = [other_data[i] for i in index]
-    sem_data = np.array(sem_data)
-    length = sem_data[:,2] * 1e-9 # m
-    width = sem_data[:,0] * 1e-9 # m
-    del other_data, index, sem_rods
+    # Now lets put every rod in the same order for SEM and fits
+    index = [ssem_footer['rods'].index(r) for r in srods]
+    ssem_data = np.array([other_data[i] for i in index])
+    slength = ssem_data[:,2] * 1e-9 # m
+    swidth = ssem_data[:,0] * 1e-9 # m
+    del other_data, index
     
-    # Now we can filter the results
-    index = np.argsort(frequency) # Remove the two lowest frequencies
-    length = length[index[2:]]
-    width = width[index[2:]]
-    frequency = frequency[index[2:]]
-    damping_time = damping_time[index[2:]]
-    quality_factor = quality_factor[index[2:]]
-    del index
+#    # Now we can filter the results
+#    index = np.argsort(sfrequency) # Remove the two lowest frequencies
+#    slength = slength[index[2:]]
+#    swidth = swidth[index[2:]]
+#    sfrequency = sfrequency[index[2:]]
+#    sdamping_time = sdamping_time[index[2:]]
+#    squality_factor = squality_factor[index[2:]]
+#    del index
     
     # Since I'll be analysing frequency vs length mostly...
-    index = np.argsort(length)
-    length = length[index]
-    width = width[index]
-    frequency = frequency[index]
-    damping_time = damping_time[index]
-    quality_factor = quality_factor[index]
+    index = np.argsort(slength)
+    slength = slength[index]
+    swidth = swidth[index]
+    sfrequency = sfrequency[index]
+    sdamping_time = sdamping_time[index]
+    squality_factor = squality_factor[index]
     del index
+    
+    # Now add all that data to a list outside the loop
+    filenames.append(sfilenames)
+    rods.append(srods)
+    fits_data.append(sfits_data)
+    fits_footer.append(sfits_footer)
+    frequency.append(sfrequency)
+    damping_time.append(sdamping_time)
+    quality_factor.append(squality_factor)
+    sem_data.append(ssem_data)
+    sem_footer.append(ssem_footer)
+    length.append(slength)
+    width.append(swidth)
+    
+del sfilenames, srods, sfits_data, sfits_footer, sfrequency, sdamping_time
+del squality_factor, ssem_data, ssem_footer, slength, swidth
 
 #%%
 
-# Prepare important data for a table 
-items = []
-for i in range(len(rods)):
-    h = '\t'.join(ivu.errorValue(sem_data[i,2], sem_data[i,3]))
-    ra = '\t'.join(ivu.errorValue(sem_data[i,4], sem_data[i,5], one_point_scale=True))
-    items.append('\t'.join([h, ra, 
-                            "{:.2f}".format(fits_data[i,0]), 
-                             "{:.1f}".format(fits_data[i,2])]))
-del i, h, ra
+### HASTA ACÁ LLEGUÉ
 
-# Make OneNote table
-heading = '\t'.join(["Longitud (nm)", "Error (nm)", 
-                     "Relación de aspecto", "Error",
-                     "Frecuencia (GHz)", "Factor de calidad"])
-items = ['\t'.join([n, r]) for n, r in zip(rods, items)]
-items = '\n'.join(items)
-heading = '\t'.join(['Rod', heading])
-table = '\n'.join([heading, items])
-ivu.copy(table)
-del heading, items
+tables = []
 
-# Save all important data to a single file
-whole_filename = os.path.join(home, r'Análisis/Resultados_Totales_{}.txt'.format(name))
-whole_data = np.array([*sem_data[:,:6].T, fits_data[:,0], 
-                       fits_data[:,1], fits_data[:,2]])
-ivs.saveTxt(whole_filename, whole_data.T, 
-            header=["Ancho (nm)", "Error (nm)",
-                    "Longitud (nm)", "Error (nm)", 
-                    "Relación de aspecto", "Error",
-                    "Frecuencia (GHz)", "Tiempo de decaimiento (ps)",
-                    "Factor de calidad"],
-            footer=dict(rods=rods, filenames=filenames),
-            overwrite=True)
-del whole_data, whole_filename
+for i, s, fs in zip(range(len(series)), series, full_series):
+    
+    # Prepare important data for a table 
+    items = []
+    for i in range(len(rods)):
+        h = '\t'.join(ivu.errorValue(sem_dataDSAGASDGGD[i,2], sem_data[i,3]))
+        ra = '\t'.join(ivu.errorValue(sem_data[i,4], sem_data[i,5], one_point_scale=True))
+        items.append('\t'.join([h, ra, 
+                                "{:.2f}".format(fits_data[i,0]), 
+                                 "{:.1f}".format(fits_data[i,2])]))
+    del i, h, ra
+    
+    # Make OneNote table
+    heading = '\t'.join(["Longitud (nm)", "Error (nm)", 
+                         "Relación de aspecto", "Error",
+                         "Frecuencia (GHz)", "Factor de calidad"])
+    items = ['\t'.join([n, r]) for n, r in zip(rods, items)]
+    items = '\n'.join(items)
+    heading = '\t'.join(['Rod', heading])
+    tables.append('\n'.join([heading, items]))
+    del heading, items
+    
+    # Save all important data to a single file
+    whole_filename = os.path.join(home, r'Análisis/Resultados_Totales_{}.txt'.format(series))
+    whole_data = np.array([*sem_data[:,:6].T, fits_data[:,0], 
+                           fits_data[:,1], fits_data[:,2]])
+    ivs.saveTxt(whole_filename, whole_data.T, 
+                header=["Ancho (nm)", "Error (nm)",
+                        "Longitud (nm)", "Error (nm)", 
+                        "Relación de aspecto", "Error",
+                        "Frecuencia (GHz)", "Tiempo de decaimiento (ps)",
+                        "Factor de calidad"],
+                footer=dict(rods=rods, filenames=filenames),
+                overwrite=True)
+    del whole_data, whole_filename
