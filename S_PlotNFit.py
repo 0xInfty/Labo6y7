092058e766b6edc2250c1ef1b42ba7475f4106c0
0xInfty <anirix.vall@gmail.com>
2019-12-09 12:11:10 -0300

    Script 'PNF'.
    Funcional:
    	Agrego parámetro 'svalues' dentro de 'fit_params' para fijar la cantidad de valores singulares.
    	Retoco el algoritmo para que si arriba se definen 't0'  o 'tf', use esos valores como tiempo inicial y final.
    	Agrego un loop al final.
    
    ¡Atención!
    	Todavía no funciona lo del tiempo inicial en el loop.

--- S_PlotNFit.py
+++ S_PlotNFit.py
@@ -14,8 +14,8 @@ import numpy as np
 #%% PARAMETERS -------------------------------------------------------------------
 
 # Parameters
-name = 'M_20191126_01'
-home = r'F:\Pump-Probe\Iván y Valeria\OneDrive\Labo 6 y 7'
+name = 'M_20191129_01'
+home = r'C:\Users\quimica\Documents\Laboratorio\Profesores\Valeria Pais\Facu\OneDrive\Labo 6 y 7'
 
 # Save parameters
 autosave = True
@@ -39,6 +39,7 @@ fit_params = dict(
         use_fraction = .2,
         choose_t0 = True,
         choose_tf = False,
+        svalues = None,
         max_svalues = 20,
         )
 fit_params = ivu.InstancesDict(fit_params)
@@ -75,15 +76,20 @@ t, V, details = ivs.loadNicePumpProbe(filename)
 # Choose time interval to fit
 if fit_params.choose_t0: # Choose initial time t0
     t0 = ivp.interactiveTimeSelector(filename, autoclose=plot_params.autoclose)
-
     t, V = iva.cropData(t0, t, V)
 else:
-    t0 = t[0]
+    try:
+        t, V = iva.cropData(t0, t, V)
+    except NameError:
+        t0 = t[0]
 if fit_params.choose_tf: # Choose final time tf
     tf = ivp.interactiveTimeSelector(filename, autoclose=plot_params.autoclose)
     t, V = iva.cropData(tf, t, V, logic='<=')
 else:
-    tf = t[-1]
+    try:
+        t, V = iva.cropData(tf, t, V)
+    except NameError:
+        tf = t[-1]
 fit_params.time_range = (t0, tf)
 del t0, tf
 
@@ -99,7 +105,10 @@ if fit_params.send_tail_to_zero:
     V0 = function(data[int( (1-fit_params.use_fraction) * len(data)):])
     del function
 else:
-    V0 = 0
+    try:
+        V0
+    except NameError:
+        V0 = 0
 data = data - V0
 fit_params.voltage_zero = V0
 del V0
@@ -107,6 +116,7 @@ del V0
 # Use linear prediction
 results, other_results, plot_results = iva.linearPrediction(
     t, data, details['dt'],
+    svalues=fit_params.svalues,
     max_svalues=fit_params.max_svalues,
     autoclose=plot_params.autoclose)
 if autosave:
@@ -136,3 +146,93 @@ del footer
 """
 tables = iva.linearPredictionTables(fit_params, results, other_results)
 ivu.copy(tables[0])
+    
+#%% LINEAR PREDICTION REPRISE
+
+nexperiments = 10
+t0 = -4.0578025641025661
+fit_params.use_full_mean = False
+fit_params.svalues = 10
+fit_params.choose_t0 = False
+overwrite = False
+
+for i in range(nexperiments):
+
+    fit_params.use_experiments = [i]
+    
+    # Load data
+    t, V, details = ivs.loadNicePumpProbe(filename)
+    
+    # Choose time interval to fit
+    if fit_params.choose_t0: # Choose initial time t0
+        t0 = ivp.interactiveTimeSelector(filename, autoclose=plot_params.autoclose)
+        t, V = iva.cropData(t0, t, V)
+    else:
+        try:
+            t, V = iva.cropData(t0, t, V)
+        except:
+            t0 = t[0]
+    if fit_params.choose_tf: # Choose final time tf
+        tf = ivp.interactiveTimeSelector(filename, autoclose=plot_params.autoclose)
+        t, V = iva.cropData(tf, t, V, logic='<=')
+    else:
+        try:
+            t, V = iva.cropData(tf, t, V)
+        except:
+            tf = t[-1]
+    fit_params.time_range = (t0, tf)
+    del t0, tf
+    
+    # Choose data to fit
+    if fit_params.use_full_mean:
+        data = np.mean(V, axis=1)
+    else:
+        data = np.mean(V[:, fit_params.use_experiments], axis=1)
+    
+    # Make a vertical shift
+    if fit_params.send_tail_to_zero:
+        function = eval('np.{}'.format(fit_params.tail_method))
+        V0 = function(data[int( (1-fit_params.use_fraction) * len(data)):])
+        del function
+    else:
+        try:
+            V0
+        except:
+            V0 = 0
+    data = data - V0
+    fit_params.voltage_zero = V0
+    del V0
+    
+    # Use linear prediction
+    results, other_results, plot_results = iva.linearPrediction(
+        t, data, details['dt'],
+        svalues=fit_params.svalues,
+        max_svalues=fit_params.max_svalues,
+        autoclose=plot_params.autoclose)
+    if autosave:
+        ivs.linearPredictionSave(filename, results, other_results, fit_params,
+                                 overwrite=overwrite)
+     
+    # Plot linear prediction
+    ivp.linearPredictionPlot(filename, plot_results, 
+                             autosave=autosave,
+                             extension=plot_params.extension,
+                             overwrite=overwrite)
+    
+    # Generate fit tables
+    """ TO LOAD OTHER FIT
+    fit_name = 'M_20191119_01'
+    
+    fit_filename = ivs.filenameToFitsFilename(fit_name, home=home)
+    results, header, footer = ivs.loadTxt(fit_filename)
+    
+    other_results_keys = ['Nsingular_values', 'chi_squared']
+    other_results = {k: footer[k] for k in other_results_keys}
+    fit_params = dict(footer)
+    for k in other_results_keys:
+        fit_params.pop(k)
+    fit_params = ivu.InstancesDict(fit_params)
+    del footer
+    """
+    tables = iva.linearPredictionTables(fit_params, results, other_results)
+    ivu.copy(tables[0])
