b0596e9f3669e3478bbc7f230bc29d4ead8b61c9
0xInfty <anirix.vall@gmail.com>
2019-12-12 23:41:33 -0300

    Scripts 'SeveralFits', 'LoadSeveralFits'. F5 + S1 + D1.
    Funcional:
    	Actualizo 'SeveralFits' para que no tenga problemas con los cambios en las funciones de los módulos.
    	Antes guardaba los datos en carpetas dentro de las carpetas de ajustes y figuras del sector 'Mediciones', pero ahora hace carpeta nueva en 'Análisis'.
    	Guardo más datos del ciclo de fiteos.
    	Agrego variable 'all_failed_groups' donde se coleccionan los grupos que no se pudieron ajustar.
    	Corro el sector que cargaba los datos ya calculados al nuevo script 'S_LoadSeveralFits.py' y lo perfecciono.
    
    Sintaxis:
    	Agrego prints.
    
    Debug:
    	Le faltaba un 'fit_params.use_full_mean = False' y por ende, por más que guardaba los datos con el grupo asignado correcto en el footer, no usaba el grupo asignado sino el promedio; de ahí que todos los resultados dieran sospechosamente iguales x'D

--- S_SeveralFits.py
+++ S_SeveralFits.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 17 14:28:53 2019
+Created on Mon Dec 09 14:28:53 2019
 
 @author: Vall
 """
@@ -17,19 +17,21 @@ import os
 #%% LINEAR PREDICTION REPRISE
 
 # Parameters
-name = 'M_20191119_01'
+name = 'M_20191129_03'
 home = r'C:\Users\Valeria\OneDrive\Labo 6 y 7'
 nexperiments = 10
 groups_mode = 'comb' # Combinations 'comb', each experiment by its own 'own'
+series = 'CombinationsOf3'
 
 # Save parameters
+path = os.path.join(home, 'Análisis', series + '_' + name)
 overwrite = False
 autosave = True
 
 # Plot parameters
 plot_params = dict(
         plot = True,
-        interactive = True,
+        interactive = False,
         autoclose = True,
         extension = '.png'
         )
@@ -38,8 +40,9 @@ plot_params = ivu.InstancesDict(plot_params)
 #%% LOAD DATA
 
 # Make filenames routs
-filename = ivs.filenameToMeasureFilename(name, home)
+filename = ivs.filenameToMeasureFilename(name, home=home)
 fit_filename = ivs.filenameToFitsFilename(name, home=home)
+other_fit_filename = os.path.join(path, name+'.txt')
 
 # Load data from a base fit made by hand
 results, header, footer = ivs.loadTxt(fit_filename)
@@ -54,6 +57,7 @@ fit_params = ivu.InstancesDict(fit_params)
 del footer
 
 # New parameters
+fit_params.use_full_mean = False
 fit_params.choose_t0 = False
 fit_params.choose_tf = False
 t0 = fit_params.time_range[0]
@@ -69,15 +73,22 @@ if groups_mode == 'comb':
 elif groups_mode == 'own':
     # Take each experiment on its own
     experiments_groups = [[i] for i in range(10)]
+print("Watch it! Loop will make " + str(len(experiments_groups)) +
+      " repetitions. Press Ctrl+C to cancel.")
 
+all_failed_groups = []
+all_groups = []
 all_results = []
 all_other_results = []
+all_fit_params = []
 #all_tables = []
-for g in experiments_groups:
+for i, g in enumerate(experiments_groups):
 
+    print("----> Loop {}/{}. Experiments: {}".format(
+            i+1, len(experiments_groups), g))
+    
     try:
     
-        print(g)
         fit_params.use_experiments = g
         
         # Load data
@@ -91,7 +102,7 @@ for g in experiments_groups:
         else:
             try:
                 t, V = iva.cropData(t0, t, V)
-            except:
+            except NameError:
                 t0 = t[0]
         if fit_params.choose_tf: # Choose final time tf
             tf = ivp.interactiveTimeSelector(filename, 
@@ -100,7 +111,7 @@ for g in experiments_groups:
         else:
             try:
                 t, V = iva.cropData(tf, t, V, logic='<=')
-            except:
+            except NameError:
                 tf = t[-1]
         fit_params.time_range = (t0, tf)
         
@@ -118,7 +129,7 @@ for g in experiments_groups:
         else:
             try:
                 V0
-            except:
+            except NameError:
                 V0 = 0
         data = data - V0
         fit_params.voltage_zero = V0
@@ -129,55 +140,32 @@ for g in experiments_groups:
             svalues=fit_params.svalues,
             autoclose=plot_params.autoclose)
         if autosave:
-            ivs.linearPredictionSave(filename, results, other_results, 
+            ivs.linearPredictionSave(other_fit_filename, results, other_results, 
                                      fit_params, overwrite=overwrite)
          
         # Plot linear prediction
-        ivp.linearPredictionPlot(filename, plot_results, 
+        ivp.linearPredictionPlot(other_fit_filename, plot_results, 
                                  autosave=autosave,
                                  extension=plot_params.extension,
                                  overwrite=overwrite)
         close()
         
-        # Generate fit tables
-#        all_tables.append(iva.linearPredictionTables(fit_params, 
-#                                                     results, 
-#                                                     other_results))
+#        # Generate fit tables
+#        tables = iva.linearPredictionTables(fit_params, 
+#                                            results, 
+#                                            other_results))
+        
+        # Save data
+        all_groups.append(g)
+        all_results.append(results)
+        all_other_results.append(other_results)
+        all_fit_params.append(fit_params)
+#        all_tables.append(tables)
+        
     except:
         
-        pass
-#        all_tables.append([None, None])
+        all_failed_groups.append(g)
+        print("Failed! :(")
 
-del t0, tf, V0
-
-#%%
-
-import os
-
-first_index = 2
-
-for i in (range(first_index, len(experiments_groups)+first_index)):
-
-# Make filenames routs
-fit_filename = ivs.filenameToFitsFilename(name, home=home)
-this_fit_filename = ( os.path.splitext(fit_filename)[0] + 
-                     '_v' + str(i) + os.path.splitext(fit_filename)[1] )
-this_fit_filename = os.path.split()[0], 'Combinaciones de a 3', os.path.split()[1]
-# Load data from a base fit made by hand
-results, header, footer = ivs.loadTxt(fit_filename)
-
-# Reorganize data
-other_results_keys = ['Nsingular_values', 'chi_squared']
-other_results = {k: footer[k] for k in other_results_keys}
-fit_params = dict(footer)
-for k in other_results_keys:
-    fit_params.pop(k)
-fit_params = ivu.InstancesDict(fit_params)
-del footer
-
-# New parameters
-fit_params.choose_t0 = False
-fit_params.choose_tf = False
-t0 = fit_params.time_range[0]
-tf = fit_params.time_range[-1]
-fit_params.svalues = other_results['Nsingular_values']
+del i, g, experiments_groups, t0, tf, V0, t, data
+del results, other_results, plot_results, fit_params, plot_params
